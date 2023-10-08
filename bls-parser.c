0044fd306172d938909aa49655a9d77d280af434
Eric Curtin <ecurtin@redhat.com>
2023-10-08 19:36:03 +0100

    changes

--- bls-parser.c
+++ bls-parser.c
@@ -1,8 +1,27 @@
 #include <stddef.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 
+#define autofree __attribute__((cleanup(cleanup_free)))
+#define autoclose __attribute__((cleanup(cleanup_close)))
 #define autofclose __attribute__((cleanup(cleanup_fclose)))
+#define autova_end __attribute__((cleanup(cleanup_va_end)))
+  
+#ifdef __cplusplus
+#define typeof decltype
+#endif
+
+#define SWAP(a, b)      \
+  do {                  \
+    typeof(a) temp = a; \
+    a = b;              \
+    b = temp;           \
+  } while (0)
+
+static inline void cleanup_free(void* p) {
+  free(*(void**)p);
+}
 
 static inline void cleanup_fclose(FILE** stream) {
   if (*stream)
@@ -14,11 +33,15 @@ typedef struct conf {
   char* bootfstype;
   char* fs;
   char* fstype;
+  char* bootfs_scoped;
+  char* bootfstype_scoped;
+  char* fs_scoped;
+  char* fstype_scoped;
 } conf;
 
-static inline char* read_conf(const char* file) {
+static inline char* read_conf(const char* file, conf* conf) {
   autofclose FILE* f = fopen(file, "r");
-  char* conf = NULL;
+  autofree char* line = NULL;
   size_t len;
 
   if (!f)
@@ -27,22 +50,23 @@ static inline char* read_conf(const char* file) {
   /* Note that /proc/cmdline will not end in a newline, so getline
    * will fail unelss we provide a length.
    */
-  if (getline(&conf, &len, f) < 0)
-    return NULL; 
+  while (getline(&line, &len, f) >= 0) {
+     if (!strcmp(line, "bootfs"))
+       SWAP(conf->bootfs_scoped, line);
+     else if (!strcmp(line, "bootfstype"))
+       SWAP(conf->bootfstype_scoped, line);
+     else if (!strcmp(line, "fs"))
+       SWAP(conf->fs_scoped, line);
+     else if (!strcmp(line, "fstype"))
+       SWAP(conf->fstype_scoped, line);
+  }
   
-  /* ... but the length will be the size of the malloc buffer, not
-   * strlen().  Fix that.
-   */
-  len = strlen(conf);
-  
-  if (conf[len - 1] == '\n')
-    conf[len - 1] = '\0';
-
-  return conf;
+  return NULL;
 }
 
 int main(void) {
-  read_conf("a.txt");
+  conf conf;
+  read_conf("a.txt", &conf);
   return 0;
 }
 
