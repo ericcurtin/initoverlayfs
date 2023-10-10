916bd7400e4df49c1100b6e9d7b18d77b02ddba9
Eric Curtin <ecurtin@redhat.com>
2023-10-10 18:09:50 +0100

    Add containerfile

--- test.sh
+++ test.sh
@@ -7,6 +7,7 @@ UNAME_M="$(uname -m)"
 if command -v distrobox-enter; then
   distrobox-enter -r centos-stream9 -- /bin/bash -c "sudo dnf install -y valgrind clang gcc erofs-utils dracut rpm-build git && cd $PWD && ./build-bin-only.sh && mkdir -p /home/ecurtin/rpmbuild/SOURCES/ && git archive -o /home/ecurtin/rpmbuild/SOURCES/initoverlayfs-$REL.tar.gz --prefix initoverlayfs-$REL/ HEAD && rpmbuild -ba *.spec && sudo mkdir -p /boot /initrofs /overlay /overlay/upper /overlay/work /initoverlayfs && sudo rpm --force -U ~/rpmbuild/RPMS/$UNAME_M/initoverlayfs-$REL-1.el9.$UNAME_M.rpm && sudo dracut -f --no-kernel && sudo initoverlayfs-install && sudo valgrind ./a.out"
 else
-  sudo podman run -it -v $PWD:$PWD centos:stream9 
+  sudo podman build -t initoverlayfs .
+  sudo podman run --rm -it -v $PWD:$PWD initoverlayfs -- /bin/bash -c "sudo dnf install -y valgrind clang gcc erofs-utils dracut rpm-build git && cd $PWD && ./build-bin-only.sh && mkdir -p /home/ecurtin/rpmbuild/SOURCES/ && git archive -o /home/ecurtin/rpmbuild/SOURCES/initoverlayfs-$REL.tar.gz --prefix initoverlayfs-$REL/ HEAD && rpmbuild -ba *.spec && sudo mkdir -p /boot /initrofs /overlay /overlay/upper /overlay/work /initoverlayfs && sudo rpm --force -U ~/rpmbuild/RPMS/$UNAME_M/initoverlayfs-$REL-1.el9.$UNAME_M.rpm && sudo dracut -f --no-kernel && sudo initoverlayfs-install && sudo valgrind ./a.out"
 fi
 
